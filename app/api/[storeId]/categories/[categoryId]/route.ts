72e4bba9b4ce1b4e76968665ef4531c3d2857767
emvalencaf <edsonmvf@gmail.com>
2023-07-17 14:35:28 -0300

    stripe

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -21,6 +21,9 @@ export async function GET(
             where: {
                 id: params.categoryId,
             },
+            include: {
+                billboard: true,
+            }
         });
 
         return NextResponse.json(category);
